7894022a6080e646c9e22941c4fa253b01bc4deb
Benjamin DalFavero <bdalfavero@gmail.com>
2025-01-21 14:03:15 -0500

    s tarted new module for maxcut-specific stuff (separate from the variational Ansatz)

--- ansatz.py
+++ ansatz.py
@@ -17,21 +17,6 @@ def beta_layer(beta: float, qubit_graph: nx.Graph) -> cirq.Circuit:
     return beta_ckt
 
 
-def maxcut_hamiltonian(qubit_graph: nx.Graph) -> cirq.PauliSum:
-    """Build a hamiltonian from the given qubit graph."""
-
-    ham = cirq.PauliSum()
-    for q1, q2 in qubit_graph.edges:
-        ham += -1.0 * edge_operator(q1, q2)
-    return ham
-
-
-def edge_operator(q1: cirq.Qid, q2: cirq.Qid) -> cirq.PauliSum:
-    """Operator from Eqn. 12 of QAOA paper. Used for the MaxCut Hamiltonian."""
-
-    return -0.5 * cirq.PauliString({q1: cirq.Z, q2: cirq.Z}) + 0.5 * cirq.PauliString()
-
-
 class CylicQAOAAnsatz:
     """Class to generate cyclic QAOA Ansatz circuits."""
 
